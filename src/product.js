4d34e44701323039286a7829cb9c130ba2b6a0fc
sivakumaru2002 <2020cs0013@svce.ac.in>
2024-02-18 14:21:30 +0530

    landing page

--- src/product.js
+++ src/product.js
@@ -1,6 +1,11 @@
 import iron from './iron man.jpg';
+import toy from './toy.jpg';
+import toy1 from './toy1.jpg';
+import mug from './mug.png';
 import mug1 from './mug1.jpg';
-
+import toy2 from './toy2.jpg';
+import doll1 from './doll1.jpg';
+import doll2 from './doll2.jpg';
 export const tank=[{
     id:1,
     src:iron,
@@ -9,43 +14,43 @@ export const tank=[{
     price:200,
   },{
     id:2,
-    src:iron,
+    src:doll1,
     description:'doll',
     name:'DOLL',
     price:400,
   },{
     id:3,
-    src:iron,
+    src:mug,
     description:'MUG',
     name:'MUG',
     price:1200,
   },{
     id:8,
-    src:iron,
+    src:toy,
     description:'TOY',
     name:'TOY',
     price:2200,
   },{
     id:4,
-    src:iron,
+    src:toy1,
     description:'TOY',
     name:'TOY',
     price:200,
   },{
     id:5,
-    src:iron,
+    src:mug1,
     description:'MUG',
     name:'MUG',
     price:200,
   },{
     id:6,
-    src:iron,
-    description:'doll',
-    name:'DOll',
+    src:toy2,
+    description:'TOY',
+    name:'TOY',
     price:200,
   },{
     id:7,
-    src:iron,
+    src:doll2,
     description:'doll',
     name:'DOll',
     price:200,
